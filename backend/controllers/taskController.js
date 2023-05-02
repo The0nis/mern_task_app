225d8402f7fec8b45297482cd3af05c5a13ddee4
TheOnis <onisamuelmichael@gmail.com>
2023-05-02 20:37:55 +0100

    done

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -22,7 +22,71 @@ const getTasks = async (req, res) => {
     }
 }
 
+//GET A SINGLE TASK
+const getTask =  async (req, res) => {
+  try {
+    const {id} =  req.params;
+    const task = await Task.findById(id)
+
+    if(!task){
+      return res.status(404).json(`No Task  with id: ${id}`)
+    }
+
+    res.status(200).json(task)
+
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
+  // console.log(req.params);
+  // res.send("Get Single Task")
+}
+
+//DELETE TASK
+const deleteTask  =  async (req, res) => {
+  try {
+    const {id} = req.params
+
+    const task = await Task.findByIdAndDelete(id);
+    if(!task){
+      return res.status(404).json(`No Task  with id: ${id}`)
+    }
+
+    res.status(200).send("Task Deleted")
+
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
+}
+
+//UPDATE A TASK
+//when using PUT all required filed must be filled, but  when using 
+//patch you  can specify the only one you want  to change
+
+//Using UPDATE
+const updateTask = async (req, res) => {
+  try {
+    const {id} = req.params
+
+    const task  = await Task.findByIdAndUpdate(
+      {_id: id}, req.body, {new: true, runValidators: true}
+    )
+
+    if(!task){
+      return res.status(404).json(`No Task  with id: ${id}`)
+    }
+
+    res.status(200).join(task)
+
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
+}
+
+
 module.exports = {
     createTask,  
     getTasks,
+    getTask,
+    deleteTask,
+    updateTask,
 };
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const Task = require("../models/taskModel");
-const { createTask, getTasks } = require("../controllers/taskController");
+const { createTask, getTasks, getTask, deleteTask, updateTask } = require("../controllers/taskController");
 const router = express.Router();
 
 router.post("/api/tasks", createTask);
-router.get("/api/tasks", getTasks)
+router.get("/api/tasks", getTasks);
+router.get("/api/tasks/:id", getTask);
+router.delete("/api/tasks/:id", deleteTask);
+//when using PUT all required filed must be filled, but  when using 
+//patch you  can specify the only one you want  to change
+//Using put
+router.put("/api/tasks/:id", updateTask);
+
+//Using Patch
+//router.patch("/api/tasks/:id", updateTask);
 
 module.exports = router;
